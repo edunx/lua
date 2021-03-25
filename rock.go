f0b2920cca9c359f1735f41e63c95ad38799b851
edunx <jianguo@eastmoney.com>
2021-03-25 17:33:03 +0800

    修改read 方法

--- rock.go
+++ rock.go
@@ -396,7 +396,7 @@ func (s *Super) Type() string                          { return "super"}
 func (s *Super) Close()                                {  }
 func (s *Super) Start() error                          { return rock_not_found }
 func (s *Super) Write( v interface{} ) error           { return rock_not_found }
-func (s *Super) Read( v interface{} ) ([]byte , error) { return nil , rock_not_found }
+func (s *Super) Read() ([]byte , error)                { return nil , rock_not_found }
 func (s *Super) Json( ) []byte                         { return rock_json_null }
 
 func IsNotFound( err error ) bool {
